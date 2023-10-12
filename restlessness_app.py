31db8b91015d6f4cbcc2c53b2c16f19e9d6112dd
Rolando <82373093+rlgvr@users.noreply.github.com>
2023-10-12 15:54:26 +0100

    Update restlessness_app.py

--- restlessness_app.py
+++ restlessness_app.py
@@ -25,6 +25,8 @@ description_and_score = st.empty()
 
 # Create a button to start processing
 if st.button("Start"):
+    all_data = []  # List to store all the data rows
+
     while data_index < len(data):
         # Get current row data
         current_row = data.iloc[data_index]
@@ -33,23 +35,32 @@ if st.button("Start"):
         z_value = current_row["z-axis (g)"]
         description = current_row["Restlessness_Description"]
         score = current_row["score_accelerometer"]
-        
+        timestamp = current_row["Timestamp_Accel"]
+
         # Update time to human-readable format
         current_time = current_row["Timestamp_Accel"]
-        
+
         # Update the table to show the x, y, z values
         values_table.table(pd.DataFrame({
             "X-axis (g)": [x_value],
             "Y-axis (g)": [y_value],
             "Z-axis (g)": [z_value]
         }))
-        
+
         # Create a description and score text
         description_text = f"Description: {description}\nScore: {score}"
-        
+
         # Update description and score text
         description_and_score.text(description_text)
-        
+
+        # Append the data for the current row to all_data list
+        all_data.append((timestamp, x_value, y_value, z_value))
+
+        # If we have more than 5 rows, update the line chart with the last 5 data points
+        if len(all_data) > 5:
+            chart_data = pd.DataFrame(all_data[-5:], columns=["Timestamp_Accel", "X-axis (g)", "Y-axis (g)", "Z-axis (g)"])
+            st.line_chart(chart_data)
+
         # Update index and wait for 1 second before the next row
         data_index += 1
         time.sleep(1)
