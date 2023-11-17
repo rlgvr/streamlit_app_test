7980f81da9dad1858855db5a99210006f7681cdc
Rolando <82373093+rlgvr@users.noreply.github.com>
2023-11-17 13:09:12 +0000

    Update restlessness_app.py

--- restlessness_app.py
+++ restlessness_app.py
@@ -17,54 +17,57 @@ def convert_timestamp_to_datetime(timestamp):
 st.title("Restlessness Evaluation")
 st.write("Start Live Evaluation")
 
-# Create a button to start processing with a unique key
-start_button_key = "start_button"
-start_button = st.button("Start", key=start_button_key)
+# Create a button to start processing
+start_button = st.button("Start")
 
-while start_button:
-    # Create an empty line chart for visualization
-    chart = st.line_chart(data=None, use_container_width=True)
+# Create an empty line chart for visualization
+chart_placeholder = st.empty()
 
-    # Get current row data
-    current_row = data.iloc[data_index]
-    x_value = current_row["x-axis (g)"]
-    y_value = current_row["y-axis (g)"]
-    z_value = current_row["z-axis (g)"]
-    description = current_row["Restlessness_Description"]
-    score = current_row["score_accelerometer"]
-    timestamp = current_row["Timestamp_Accel"]
+# Create a table to show the x, y, z values (initially empty)
+values_table = st.empty()
 
-    # Update time to human-readable format
-    current_time = current_row["Timestamp_Accel"]
+# Create a text element for the description and score (initially empty)
+description_and_score = st.empty()
 
-    # Update the table to show the x, y, z values
-    st.table(pd.DataFrame({
-        "X-axis (g)": [x_value],
-        "Y-axis (g)": [y_value],
-        "Z-axis (g)": [z_value]
-    }))
+# Check if the button is pressed
+if start_button:
+    while data_index < len(data):
+        # Get current row data
+        current_row = data.iloc[data_index]
+        x_value = current_row["x-axis (g)"]
+        y_value = current_row["y-axis (g)"]
+        z_value = current_row["z-axis (g)"]
+        description = current_row["Restlessness_Description"]
+        score = current_row["score_accelerometer"]
+        timestamp = current_row["Timestamp_Accel"]
 
-    # Create a description and score text
-    description_text = f"Description: {description}\nScore: {score}"
+        # Update time to human-readable format
+        current_time = current_row["Timestamp_Accel"]
 
-    # Update description and score text
-    st.text(description_text)
+        # Update the table to show the x, y, z values
+        values_table.table(pd.DataFrame({
+            "X-axis (g)": [x_value],
+            "Y-axis (g)": [y_value],
+            "Z-axis (g)": [z_value]
+        }))
 
-    # Append data to the DataFrame to plot
-    data_to_plot = pd.DataFrame({
-        "Timestamp_Accel": [timestamp],
-        "X-axis (g)": [x_value],
-        "Y-axis (g)": [y_value],
-        "Z-axis (g)": [z_value]
-    })
+        # Create a description and score text
+        description_text = f"Description: {description}\nScore: {score}"
 
-    if data_index >= 5:
+        # Update description and score text
+        description_and_score.text(description_text)
+
+        # Append data to the DataFrame to plot
+        data_to_plot = pd.DataFrame({
+            "Timestamp_Accel": [timestamp],
+            "X-axis (g)": [x_value],
+            "Y-axis (g)": [y_value],
+            "Z-axis (g)": [z_value]
+        })
+
         # Update the line chart with the new data
-        chart.line_chart(data_to_plot, use_container_width=True, x="Timestamp_Accel", y=["X-axis (g)", "Y-axis (g)", "Z-axis (g)"])
+        chart_placeholder.line_chart(data_to_plot, use_container_width=True, x="Timestamp_Accel", y=["X-axis (g)", "Y-axis (g)", "Z-axis (g)"])
 
-    # Update index and wait for 1 second before the next row
-    data_index += 1
-    time.sleep(1)
-
-    # Check if the button is still pressed
-    start_button = st.button("Start", key=start_button_key)
+        # Update index and wait for 1 second before the next row
+        data_index += 1
+        time.sleep(1)
