7289630a86cb9b76d1d29fea6bb53022091a713b
Rolando <82373093+rlgvr@users.noreply.github.com>
2023-11-17 13:00:45 +0000

    Update restlessness_app.py

--- restlessness_app.py
+++ restlessness_app.py
@@ -17,55 +17,47 @@ def convert_timestamp_to_datetime(timestamp):
 st.title("Restlessness Evaluation")
 st.write("Start Live Evaluation")
 
-# Create a table to show the x, y, z values (initially empty)
-values_table = st.empty()
+# Create an empty line chart for visualization
+chart = st.line_chart(data=None, use_container_width=True)
 
-# Create a text element for the description and score (initially empty)
-description_and_score = st.empty()
+while st.button("Start"):
+    # Get current row data
+    current_row = data.iloc[data_index]
+    x_value = current_row["x-axis (g)"]
+    y_value = current_row["y-axis (g)"]
+    z_value = current_row["z-axis (g)"]
+    description = current_row["Restlessness_Description"]
+    score = current_row["score_accelerometer"]
+    timestamp = current_row["Timestamp_Accel"]
 
-# Create a button to start processing
-if st.button("Start"):
-    # Create an empty line chart for visualization
-    chart = st.line_chart(data=None, use_container_width=True)
+    # Update time to human-readable format
+    current_time = current_row["Timestamp_Accel"]
 
-    data_to_plot = pd.DataFrame(columns=["Timestamp_Accel", "X-axis (g)", "Y-axis (g)", "Z-axis (g)"])
-    while data_index < len(data):
-        # Get current row data
-        current_row = data.iloc[data_index]
-        x_value = current_row["x-axis (g)"]
-        y_value = current_row["y-axis (g)"]
-        z_value = current_row["z-axis (g)"]
-        description = current_row["Restlessness_Description"]
-        score = current_row["score_accelerometer"]
-        timestamp = current_row["Timestamp_Accel"]
+    # Update the table to show the x, y, z values
+    st.table(pd.DataFrame({
+        "X-axis (g)": [x_value],
+        "Y-axis (g)": [y_value],
+        "Z-axis (g)": [z_value]
+    }))
 
-        # Update time to human-readable format
-        current_time = current_row["Timestamp_Accel"]
+    # Create a description and score text
+    description_text = f"Description: {description}\nScore: {score}"
 
-        # Update the table to show the x, y, z values
-        values_table.table(pd.DataFrame({
-            "X-axis (g)": [x_value],
-            "Y-axis (g)": [y_value],
-            "Z-axis (g)": [z_value]
-        }))
+    # Update description and score text
+    st.text(description_text)
 
-        # Create a description and score text
-        description_text = f"Description: {description}\nScore: {score}"
+    # Append data to the DataFrame to plot
+    data_to_plot = pd.DataFrame({
+        "Timestamp_Accel": [timestamp],
+        "X-axis (g)": [x_value],
+        "Y-axis (g)": [y_value],
+        "Z-axis (g)": [z_value]
+    })
 
-        # Update description and score text
-        description_and_score.text(description_text)
+    if data_index >= 5:
+        # Update the line chart with the new data
+        chart.line_chart(data_to_plot, use_container_width=True, x="Timestamp_Accel", y=["X-axis (g)", "Y-axis (g)", "Z-axis (g)"])
 
-        # Append data to the DataFrame to plot
-        data_to_plot = pd.concat([data_to_plot, pd.DataFrame({
-            "Timestamp_Accel": [timestamp],
-            "X-axis (g)": [x_value],
-            "Y-axis (g)": [y_value],
-            "Z-axis (g)": [z_value]
-        })], ignore_index=True)
-
-        if data_index >= 5:
-            chart.line_chart(data_to_plot, use_container_width=True, x="Timestamp_Accel", y=["X-axis (g)", "Y-axis (g)", "Z-axis (g)"])
-        
-        # Update index and wait for 1 second before the next row
-        data_index += 1
-        time.sleep(1)
+    # Update index and wait for 1 second before the next row
+    data_index += 1
+    time.sleep(1)
